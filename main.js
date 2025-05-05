fb8db1769d8b4ceae0672fe598eb1f70fd16ca25
Ulrich <soukpoagoulrich@gmail.com>
2025-05-05 17:11:07 +0100

    suppression fichiers intrus

--- main.js
+++
@@ -1,5 +0,0 @@
-// Exemple : menu simple pour mobile
-const nav = document.querySelector('nav');
-document.querySelector('#hero p').addEventListener('click', () => {
-  nav.classList.toggle('open');
-});
